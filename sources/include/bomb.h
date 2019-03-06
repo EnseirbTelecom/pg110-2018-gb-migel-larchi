d9eaa00eff6188d89534caa5d904464aa8356818
MIGEL ARACHCHIGE Damien <dmigelarach@panisse.pedago.ipb.fr>
2019-03-06 11:00:57 +0100

    gestion de bombe:   -definition de la structure bomb   -initialisation et suppression   -affichage d'un bombe

---
+++ sources/include/bomb.h
@@ -0,0 +1,10 @@
+#include <player.h>
+
+struct bomb;
+
+//creates a bomb
+struct bomb* bomb_init(struct player* player);
+void bomb_free(struct bomb* bomb);
+
+// Display bomb on the screen
+void bomb_display(struct bomb* bomb);
---
+++ sources/src/bomb.c
@@ -0,0 +1,43 @@
+#include <SDL/SDL_image.h>
+#include <assert.h>
+
+#include <misc.h>
+#include <window.h>
+#include <player.h>
+#include <sprite.h>
+#include <constant.h>
+
+
+struct bomb{
+  int x,y;
+  int TIME;
+  struct player* player;
+};
+
+struct bomb* bomb_init(struct player* player){
+  struct bomb* bomb=malloc(sizeof(*bomb));
+  if (!bomb)
+    error("Memory error");
+
+  bomb->x=player_get_x(player);
+  bomb->y=player_get_y(player);
+  bomb->TIME=3;
+  bomb->player=player;
+  return bomb;
+};
+
+
+void bomb_free(struct bomb* bomb){
+  assert(bomb);
+  free(bomb);
+};
+
+void bomb_list_free(struct bomb_list* bombs) {
+  assert(bombs);
+  free(bombs);
+}
+void bomb_display(struct bomb* bomb){
+  assert(bomb);
+  window_display_image(sprite_get_bomb(bomb->TIME),
+  (bomb->x)* SIZE_BLOC,(bomb->y)* SIZE_BLOC);
+};
