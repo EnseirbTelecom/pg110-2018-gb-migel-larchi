174c7624852c83f4df47caae55fc9b74d1aa94ae
MIGEL ARACHCHIGE Damien <dmigelarach@panisse.pedago.ipb.fr>
2019-03-08 10:30:52 +0100

    changement des notations + deplace réellement les box

--- sources/src/myfonct.c
+++ sources/src/myfonct.c
@@ -4,32 +4,33 @@
 #include <constant.h>
 
 int boxe_move(enum direction direction,struct map* map,int x,int y){
-  int x2,y2;
-  x2=x;
-  y2=y;
+  int x1,y1;
+  x1=x;
+  y1=y;
+
   switch (direction) {
     case SOUTH:
-      y2=y+1;
+      y1=y+1;
     break;
 
     case NORTH:
-      y2=y-1;
+      y1=y-1;
     break;
 
     case WEST:
-      x2=x-1;
+      x1=x-1;
     break;
 
     case EAST:
-      x2=x+1;
+      x1=x+1;
     break;
   }
-  if (!map_is_inside(map, x2, y2))
+  if (!map_is_inside(map, x1, y1))
 		return 0;
 
-  if(map_get_cell_type(map,x2,y2)==CELL_EMPTY){
+  if(  map_get_cell_type(map,x1,y1)==CELL_EMPTY){
+    map_set_cell_type(map, x1, y1, map_get_cell_type(map,x,y));
     map_set_cell_type(map, x, y, CELL_EMPTY);
-    map_set_cell_type(map, x2, y2, CELL_BOX);
     return 1;
   }
   return 0;
