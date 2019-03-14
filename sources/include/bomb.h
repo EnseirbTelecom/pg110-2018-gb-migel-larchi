b663bdcf6ccd9e05c18f807d48cc202597b0ec58
MIGEL ARACHCHIGE Damien <dmigelarach@panisse.pedago.ipb.fr>
2019-03-14 17:08:56 +0100

    ajout de commentaire

--- sources/include/bomb.h
+++ sources/include/bomb.h
@@ -6,13 +6,24 @@ struct bomb;
 struct bomb* bomb_init(struct player* player);
 void bomb_free(struct bomb* bomb);
 
+// Get bomb->player
+struct player *bomb_get_player(struct bomb *bomb);
+
+//get bomb->player
+int bomb_get_exploded(struct bomb *bomb);
+
+//set bomb->exploded
+void bomb_set_exploded(struct bomb *bomb,int exploded);
+
+void bomb_explosion_end(struct map *map,struct bomb* bomb);
+
 // Display bomb on the screen
 void bomb_display(struct map* map,struct bomb* bomb);
 
 //get bomb state
 int bomb_get_state(struct bomb* bomb);
 
-//Display bomb explosion
-void bomb_explosion_display(struct map* map,struct bomb* bomb);
+//set bomb explosion on map
+void bomb_explosion_map_set(struct map* map,struct bomb* bomb);
 
-void bomb_explosion_display_cell(struct map* map,int x,int y);
+int bomb_explosion_map_set_cell(struct map* map,int x,int y);
