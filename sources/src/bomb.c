f75b8fae8966108c03596bc069c5650ecfcf59d7
MIGEL ARACHCHIGE Damien <dmigelarach@panisse.pedago.ipb.fr>
2019-03-14 17:20:54 +0100

    affichage des explosions

--- sources/src/bomb.c
+++ sources/src/bomb.c
@@ -65,45 +65,230 @@ int bomb_get_state(struct bomb* bomb){
   return 4-state;
 };
 
-void bomb_display(struct map* map,struct bomb* bomb){
+void bomb_display(struct map *map,struct bomb *bomb){
   assert(bomb);
-  int state=bomb_get_state(bomb);
-  window_display_image(sprite_get_bomb(state),
-  (bomb->x)* SIZE_BLOC,(bomb->y)* SIZE_BLOC);
-  if(state==0)
-    bomb_explosion_display(map,bomb);
-};
-
-void bomb_explosion_display_cell(struct map* map,int x,int y) {
-    enum cell_type cell_type = map_get_cell_type(map,x,y);
-    switch (cell_type) {
-      case CELL_EMPTY:
-      window_display_image(sprite_get_bomb(0),x*SIZE_BLOC,y*SIZE_BLOC);
-      break;
-      case CELL_BOX:
-      break;
-      case CELL_KEY:
-      break;
-      case CELL_BONUS:
-      break;
-      case CELL_MONSTER:
-      break;
-      case CELL_BOMB:
-      break;
-      case CELL_SCENERY:
-      break;
-      case CELL_DOOR:
-      break;
-    }
-}
-
-void bomb_explosion_display(struct map* map,struct bomb* bomb){
   int x=bomb->x;
   int y=bomb->y;
-  for (int i = 1; i < 2; i++) {
-    bomb_explosion_display_cell(map,x,y+i);
-    bomb_explosion_display_cell(map,x,y-i);
-    bomb_explosion_display_cell(map,x+i,y);
-    bomb_explosion_display_cell(map,x-i,y);
+  int state=bomb_get_state(bomb);
+  if(state==0 && (bomb->exploded)==0){
+    bomb_explosion_map_set(map,bomb);
+    bomb->exploded=1;
+  }else if (state==0 && (bomb->exploded)==2) {
+    bomb_explosion_map_set(map,bomb);
+    bomb->exploded=1;
+  }
+  window_display_image(sprite_get_bomb(state),x* SIZE_BLOC,y* SIZE_BLOC);
+};
+
+
+int bomb_explosion_map_set_cell(struct map *map,int x,int y) {
+    enum cell_type cell_type = map_get_cell_type(map,x,y);
+
+    switch (cell_type) {
+      case CELL_EMPTY:
+        map_set_cell_type(map,x,y,CELL_EXPLOSION);
+        return 1;
+        break;
+
+
+      case CELL_KEY:
+        break;
+
+      case CELL_BONUS:
+        break;
+
+      case CELL_MONSTER:
+        break;
+
+      case CELL_SCENERY:
+        break;
+
+      case CELL_DOOR:
+        break;
+
+      case CELL_EXPLOSION:
+        return 1;
+        //window_display_image(sprite_get_bomb(0),x*SIZE_BLOC,y*SIZE_BLOC);
+      break;
+
+      case CELL_BOMB:
+        return 1;
+        break;
+
+    case CELL_BOX:
+      map_set_cell_type(map,x,y,CELL_EXPLOSION);
+      return 0;
+      break;
+    }
+  return 0;
+}
+
+int bomb_explosion_map_set_cell_2(struct map *map,int x,int y) {
+    enum cell_type cell_type = map_get_cell_type(map,x,y);
+
+    switch (cell_type) {
+      case CELL_EMPTY:
+        map_set_cell_type(map,x,y,CELL_EXPLOSION);
+        return 1;
+        break;
+
+
+      case CELL_KEY:
+        break;
+
+      case CELL_BONUS:
+        break;
+
+      case CELL_MONSTER:
+        break;
+
+      case CELL_SCENERY:
+        break;
+
+      case CELL_DOOR:
+        break;
+
+      case CELL_EXPLOSION:
+        return 1;
+        //window_display_image(sprite_get_bomb(0),x*SIZE_BLOC,y*SIZE_BLOC);
+      break;
+
+      case CELL_BOMB:
+        return 1;
+        break;
+
+    case CELL_BOX:
+      return 0;
+      break;
+    }
+  return 0;
+}
+
+void bomb_explosion_map_set(struct map* map,struct bomb* bomb){
+
+  int x=bomb->x;
+  int y=bomb->y;
+
+  int xi=x+1;
+  int x_i=x-1;
+  int yi=y+1;
+  int y_i=y-1;
+
+  if (bomb_get_exploded(bomb)==0) {
+    int i=0;
+    while (i<portee) {
+      if(map_is_inside(map,x,yi) && bomb_explosion_map_set_cell(map,x,yi)){
+        yi++;
+        i++;
+      }else{
+        break;
+      }
+    }
+
+    i=0;
+    while (i<portee) {
+      if(map_is_inside(map,x,y_i) && bomb_explosion_map_set_cell(map,x,y_i)){
+        y_i--;
+        i++;
+      }else{
+        break;
+      }
+    }
+
+    i=0;
+    while (i<portee) {
+      if(map_is_inside(map,xi,y) && bomb_explosion_map_set_cell(map,xi,y)){
+        xi++;
+        i++;
+      }else{
+        break;
+      }
+    }
+
+    i=0;
+    while (i<portee) {
+      if(map_is_inside(map,x_i,y) && bomb_explosion_map_set_cell(map,x_i,y)){
+        x_i--;
+        i++;
+      }else{
+        break;
+      }
+    }
+  }
+  else if (bomb_get_exploded(bomb)==2) {
+    int i=0;
+    while (i<portee) {
+      if(map_is_inside(map,x,yi) && bomb_explosion_map_set_cell_2(map,x,yi)){
+        yi++;
+        i++;
+      }else{
+        break;
+      }
+    }
+
+    i=0;
+    while (i<portee) {
+      if(map_is_inside(map,x,y_i) && bomb_explosion_map_set_cell_2(map,x,y_i)){
+        y_i--;
+        i++;
+      }else{
+        break;
+      }
+    }
+
+    i=0;
+    while (i<portee) {
+      if(map_is_inside(map,xi,y) && bomb_explosion_map_set_cell_2(map,xi,y)){
+        xi++;
+        i++;
+      }else{
+        break;
+      }
+    }
+
+    i=0;
+    while (i<portee) {
+      if(map_is_inside(map,x_i,y) && bomb_explosion_map_set_cell_2(map,x_i,y)){
+        x_i--;
+        i++;
+      }else{
+        break;
+      }
+    }
+  }
+
+};
+
+void bomb_explosion_end(struct map *map,struct bomb* bomb){
+  assert(bomb);
+  assert(map);
+
+  int x=bomb->x;
+  int y=bomb->y;
+
+  int xi=x+1;
+  int x_i=x-1;
+  int yi=y+1;
+  int y_i=y-1;
+  map_set_cell_type(map,x,y,CELL_EMPTY);
+  for ( int i=0 ; i < portee; i++) {
+    if(map_is_inside(map,x,yi) && (map_get_cell_type(map,x,yi)==CELL_EXPLOSION|| map_get_cell_type(map,x,yi)==CELL_BOMB)){
+      map_set_cell_type(map,x,yi,CELL_EMPTY);
+      yi++;
+    }
+
+    if(map_is_inside(map,xi,y) && (map_get_cell_type(map,xi,y)==CELL_EXPLOSION|| map_get_cell_type(map,xi,y)==CELL_BOMB)){
+      map_set_cell_type(map,xi,y,CELL_EMPTY);
+      xi++;
+    }
+
+    if(map_is_inside(map,x,y_i) && (map_get_cell_type(map,x,y_i)==CELL_EXPLOSION|| map_get_cell_type(map,x,y_i)==CELL_BOMB)){
+      map_set_cell_type(map,x,y_i,CELL_EMPTY);
+      y_i--;
+    }
+    if(map_is_inside(map,x_i,y) && (map_get_cell_type(map,x_i,y)==CELL_EXPLOSION|| map_get_cell_type(map,x_i,y)==CELL_BOMB)){
+      map_set_cell_type(map,x_i,y,CELL_EMPTY);
+      x_i--;
+    }
   }
 };
