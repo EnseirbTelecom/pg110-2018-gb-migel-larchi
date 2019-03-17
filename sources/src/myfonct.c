f153551bf01e7a2596bfe81fd3d8d0598d55701f
dmigelarach <43379149+dmigelarach@users.noreply.github.com>
2019-03-17 18:34:21 +0100

    correction le deplacement des bonus gère les bonus caché

--- sources/src/myfonct.c
+++ sources/src/myfonct.c
@@ -5,9 +5,8 @@
 #include <constant.h>
 
 int boxe_move(enum direction direction,struct map* map,int x,int y){
-  int x1,y1;
-  x1=x;
-  y1=y;
+  int x1=x, y1=y;
+  enum bonus_type bonus_type = map_get_bonus_type(map,x,y);
 
   switch (direction) {
     case SOUTH:
@@ -31,6 +30,7 @@ int boxe_move(enum direction direction,struct map* map,int x,int y){
 
   if(  map_get_cell_type(map,x1,y1)==CELL_EMPTY ){
     map_set_cell_type(map, x1, y1, map_get_cell_type(map,x,y));
+    map_set_bonus_type(map,x1,y1,bonus_type);
     map_set_cell_type(map, x, y, CELL_EMPTY);
     return 1;
   }
