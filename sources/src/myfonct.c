6bb60c35260e377632b1cf02bcb40cbc8d8ff396
dmigelarach <43379149+dmigelarach@users.noreply.github.com>
2019-03-17 18:18:38 +0100

    ??

--- sources/src/myfonct.c
+++ sources/src/myfonct.c
@@ -29,7 +29,7 @@ int boxe_move(enum direction direction,struct map* map,int x,int y){
   if (!map_is_inside(map, x1, y1))
 		return 0;
 
-  if(  map_get_cell_type(map,x1,y1)==CELL_EMPTY){
+  if(  map_get_cell_type(map,x1,y1)==CELL_EMPTY ){
     map_set_cell_type(map, x1, y1, map_get_cell_type(map,x,y));
     map_set_cell_type(map, x, y, CELL_EMPTY);
     return 1;
